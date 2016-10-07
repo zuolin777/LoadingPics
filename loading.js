391d0c5d6d8f22bf7a31530e19f8fcc3c9b44148
zuolin <zuolin777@126.com>
2016-10-07 23:26:45 +0800

    loading.js

---
+++ loading.js
@@ -0,0 +1,31 @@
+/*
+*图片预加载 zl
+*/
+function loading(arr,callBack){
+	var arrAllImg = [];
+	var count = 0;
+	if(!(arr instanceof Array)){
+		throw new Error(arr + ' is not an Array');
+		return;
+	}
+	for(var i = 0; i < arr.length; i ++){
+		var img = new Image();
+		img.src = arr[i];
+		arrAllImg.push(img);
+	}
+	for (var i = 0; i < arrAllImg.length; i++) {
+		arrAllImg[i].onload = function(){
+			count ++;
+			if(count == arrAllImg.length){
+				if(callBack instanceof Function){
+					callBack(arrAllImg);					
+				}else{
+					throw new Error(callBack + ' is not a function');
+				}
+
+			}
+		}
+	}
+}
+
+
